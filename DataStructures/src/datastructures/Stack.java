42325a1f023c0644d7feda1b9d83cc2d04421a93
surya <surya@SuryaLinuxMintLenovo>
2017-01-12 21:56:46 +0530

    Completed LinkedList and formatted others

--- DataStructures/src/datastructures/Stack.java
+++ DataStructures/src/datastructures/Stack.java
@@ -1,107 +1,81 @@
 package datastructures;
 
-
 public class Stack {
 
-	
-	
-	int length=0;
-	
-	String name="";
+	int length = 0;
+
+	String name = "";
 
 	Node top;
-	Stack(String name)
-	{
-		this.name=name;
+
+	Stack(String name) {
+		this.name = name;
 	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 
-		Stack s= new Stack("hello");
-		
-	System.out.println(s.isEmpty());
-	s.push("1");
-	s.push("2");
-	System.out.println(s.peek());
-	s.push("4");
-	s.pop();
-	s.push("3");
-	System.out.println(s.isEmpty());
-		
-		for(int i=0;!s.isEmpty();i++)
-		{
+		Stack s = new Stack("hello");
+
+		System.out.println(s.isEmpty());
+		s.push("1");
+		s.push("2");
+		System.out.println(s.peek());
+		s.push("4");
+		s.pop();
+		s.push("3");
+		System.out.println(s.isEmpty());
+
+		for (int i = 0; !s.isEmpty(); i++) {
 			System.out.println(s.peek());
-			
+
 			s.pop();
 		}
-		
-		
-		
-		
 
-
-		
-		
 	}
-	
-	boolean isEmpty()
-	{
-		if(this.length==0)
-		return true;
-		
+
+	boolean isEmpty() {
+		if (this.length == 0)
+			return true;
+
 		return false;
-		
+
 	}
-	
-	public void push(Object x)
-	{
-		if(this.isEmpty())
-		{
+
+	public void push(Object x) {
+		if (this.isEmpty()) {
 			length++;
-			top=new Node(null,x);
+			top = new Node(null, x);
 			return;
 		}
 		length++;
-		top=new Node(top,x);
-		
+		top = new Node(top, x);
+
 	}
-	
-	
-	public void pop()
-	{
-		
-		top=top.previous;	
+
+	public void pop() {
+
+		top = top.previous;
 		length--;
-		
-		
+
 	}
-	
-	
-	public Object peek()
-	{
-		
+
+	public Object peek() {
+
 		return top.value;
 	}
-	
 
 }
 
- class Node{
-	
-	
+class Node {
 
 	Node previous;
 	Object value;
-	
-	Node(Node prev, Object value )
-	{
-		this.previous=prev;
-		this.value=value;
+
+	Node(Node prev, Object value) {
+		this.previous = prev;
+		this.value = value;
 
 	}
-	
-	
-	
-	
+
 }
