1d07c7bc8dca576828fb007a129547434d3c8e03
surya <surya@SuryaLinuxMintLenovo>
2017-01-31 23:52:46 +0530

    all utils

---
+++ DataStructures/src/sources/AllUtils.java
@@ -0,0 +1,40 @@
+package sources;
+
+public class AllUtils {
+	
+	
+	int[] array;
+	int size;
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println(Math.random());
+	}
+	
+	
+	AllUtils(int size)
+	{
+		this.size=size;
+		this.array = new int[size];
+		
+	}
+	
+	
+	void generateRandomArray()
+	{
+		for(int i=0;i<this.array.length;i++)
+;
+		}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
