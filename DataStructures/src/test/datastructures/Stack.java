c54f378e34dffa1da1e70d9bda0069d1f9640b45
surya <surya@SuryaLinuxMintLenovo.com>
2017-02-13 22:14:28 +0530

    creating standard stack

--- DataStructures/src/test/datastructures/Stack.java
+++ DataStructures/src/test/datastructures/Stack.java
@@ -1,81 +1,129 @@
 package test.datastructures;
 
-public class Stack {
+import java.util.Scanner;
 
-	int length = 0;
 
-	String name = "";
+public class Stack<E> {
 
-	Node top;
+	int top = -1;
+	E[] stack;
 
-	Stack(String name) {
-		this.name = name;
+	Stack(int size) {
+		this.stack = (E[]) new Object[size];
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Stack<Character> balancedBracket = new Stack<Character>(100000);
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for (int a0 = 0; a0 < t; a0++) {
+			String s = in.next();
+			balancedBracket = new Stack<Character>(100000);
+			balancedBracket.bracketOperations(s);
 
-		Stack s = new Stack("hello");
-
-		System.out.println(s.isEmpty());
-		s.push("1");
-		s.push("2");
-		System.out.println(s.peek());
-		s.push("4");
-		s.pop();
-		s.push("3");
-		System.out.println(s.isEmpty());
-
-		for (int i = 0; !s.isEmpty(); i++) {
-			System.out.println(s.peek());
-
-			s.pop();
 		}
 
 	}
 
-	boolean isEmpty() {
-		if (this.length == 0)
-			return true;
+	public void bracketOperations(String s) {
 
-		return false;
-
-	}
-
-	public void push(Object x) {
-		if (this.isEmpty()) {
-			length++;
-			top = new Node(null, x);
+		int length = s.length();
+		Character c;
+		Character ithChar = s.charAt(length - 1);
+		if (ithChar == '{' || ithChar == '[' || ithChar == '(') {
+			System.out.println("NO");
 			return;
 		}
-		length++;
-		top = new Node(top, x);
+
+		for (int i = 0; i < length; i++) {
+
+			ithChar = s.charAt(i);
+
+			if (ithChar == '{' || ithChar == '[' || ithChar == '(') {
+				c = s.charAt(i);
+				this.push((E) c);
+			} else if (isEmpty()) {
+				System.out.println("NO");
+				return;
+			} else if (ithChar == '}' || ithChar == ']' || ithChar == ')') {
+				c = (Character) this.peek();
+
+				if (ithChar == '}' && c != '{') {
+					System.out.println("NO");
+					return;
+				} else if (ithChar == ']' && c != '[') {
+					System.out.println("NO");
+					return;
+				} else if (ithChar == ')' && c != '(') {
+					System.out.println("NO");
+					return;
+				}
+
+				this.pop();
+
+			}
+
+		}
+		System.out.println("YES");
 
 	}
 
-	public void pop() {
+	public boolean isEmpty() {
+		if (this.top < 0)
+			return true;
+		return false;
+	}
 
-		top = top.previous;
-		length--;
+	public static void maximumElement() {
+
+		Stack stac = new Stack(100000);
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int oper = 0;
+		for (int i = 0; i < n; i++) {
+			oper = s.nextInt();
+
+			if (oper == 1) {
+				stac.push((Integer) s.nextInt());
+			} else if (oper == 2) {
+				stac.pop();
+			} else if (oper == 3) {
+				stac.printMax();
+			}
+
+		}
 
 	}
 
-	public Object peek() {
-
-		return top.value;
+	void push(E value) {
+		this.stack[++top] = value;
 	}
 
-}
+	E peek() {
+		if (top < 0)
+			return null;
+		else
+			return this.stack[top];
+	}
 
-class Node {
+	void pop() {
+		if (top < 0)
+			return;
+		this.stack[top] = null;
+		top--;
+	}
 
-	Node previous;
-	Object value;
+	void printMax() {
+		// System.out.println(this.stack);
+		if (this.stack[0] instanceof Integer) {
+			Integer max = 0;
+			for (int i = 0; i <= top; i++) {
+				if (max < (Integer) stack[i])
+					max = (Integer) stack[i];
+			}
 
-	Node(Node prev, Object value) {
-		this.previous = prev;
-		this.value = value;
-
+			System.out.println(max);
+		}
 	}
 
 }
