f8d8a4e833b92f778cc278f39d406615ad904311
surya <surya@SuryaLinuxMintLenovo.com>
2017-02-25 00:52:19 +0530

    hello world

---
+++ DataStructures/src/test/algorithms/dynamic/GenerateBinaryNumbers.java
@@ -0,0 +1,27 @@
+package test.algorithms.dynamic;
+
+import java.util.Arrays;
+
+public class GenerateBinaryNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+		int n=100;
+		String[] a= new String[n];
+		a[0]="0";
+		a[1]="1";
+		
+		for(int i=2;i<n;i++){
+			
+			a[i]=a[i/2]+""+i%2;
+		}
+		System.out.println(Arrays.toString(a));
+		
+	}
+	
+	
+
+
+}
