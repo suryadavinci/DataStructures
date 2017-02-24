8f58f719e8ccb6a3ad52c20131f1aa1ed5879928
gnanasuriyan.g <gnanasuriyan.g@D7240.prodapt.com>
2017-02-24 17:46:44 +0530

    unique paths using dynamic algorithm done

---
+++ DataStructures/src/test/algorithms/dynamic/UniquePaths.java
@@ -0,0 +1,95 @@
+package test.algorithms.dynamic;
+
+import java.util.Arrays;
+
+public class UniquePaths {
+	int n;
+	int m;
+	public static long mem[][];
+	int count;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UniquePaths up = new UniquePaths();
+		up.n = 20;
+		up.m = 20;
+		up.mem = new long[up.n][up.m];
+		
+		
+		System.out.println(up.totalPaths(0, 0));
+		
+		System.out.println(up.totalNonDynamicPaths(0, 0));
+		
+
+	}
+
+	public void uniquePaths(int row, int col) {
+		
+		if (row == n - 1 && col == m - 1) {
+			count++;
+			System.out.print("(" + row + "," + col + ")-->");
+			System.out.println(count);
+			return;
+		} else {
+
+			if (row < n - 1) {
+				System.out.print("(" + row + "," + col + ")-->");
+
+				uniquePaths(row + 1, col);
+			}
+			if (col < m - 1) {
+				System.out.print("(" + row + "," + col + ")-->");
+
+				uniquePaths(row, col + 1);
+			}
+		}
+	}
+
+	public long totalPaths(int row, int col) {
+		long count=0;
+		if (row == n - 1 && col == m - 1) {
+			count++;
+			return 1;
+		} else {
+			
+			if(mem[row][col]!=0){
+				return mem[row][col];
+			}
+			
+			if (row < n - 1) {
+				count=count+totalPaths(row + 1, col);
+			}
+			if (col < m - 1) {
+				count=count+totalPaths(row, col + 1);
+			}
+		}
+		
+		mem[row][col]=count;
+		return count;
+	}
+	
+	
+	public long totalNonDynamicPaths(int row, int col) {
+		long count=0;
+		if (row == n - 1 && col == m - 1) {
+			count++;
+			return 1;
+		} else {
+			
+			
+			
+			if (row < n - 1) {
+				count=count+totalNonDynamicPaths(row + 1, col);
+			}
+			if (col < m - 1) {
+				count=count+totalNonDynamicPaths(row, col + 1);
+			}
+		}
+		
+		return count;
+	}
+
+	public void bottomUp() {
+		
+	}
+
+}
