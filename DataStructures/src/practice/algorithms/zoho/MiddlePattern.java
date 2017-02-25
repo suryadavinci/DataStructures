821af7c3c78d1a3508623ff8950c43e3326a5a22
surya <surya@SuryaLinuxMintLenovo.com>
2017-02-25 09:31:28 +0530

    pattern

---
+++ DataStructures/src/practice/algorithms/zoho/MiddlePattern.java
@@ -0,0 +1,57 @@
+package practice.algorithms.zoho;
+
+import java.util.Arrays;
+
+public class MiddlePattern {
+
+	public static void main(String[] a){
+		String s="PROGRAM";
+		
+		char[] c=s.toCharArray();
+		
+		int mid=c.length/2;
+		int i=mid;
+		StringBuilder sout=new StringBuilder("");
+		int l=c.length*2;
+
+		while(i<s.length() && i!=mid-1){
+			if(i<s.length() && i>=mid)
+			{
+				
+				sout.append(s.charAt(i));
+				System.out.println(getSpaces(l)+sout);
+				l=l-2;
+				i++;
+			}
+			
+
+			if(i==c.length)
+			{
+				i=0;
+			}
+			if(i<mid){
+				sout.append(s.charAt(i));
+				System.out.println(getSpaces(l--)+sout);
+				l=l-2;
+				i++;
+				if(i==mid-1)
+					{
+					sout.append(s.charAt(mid-1));
+					break;
+					}
+			}
+			
+		}
+
+		System.out.println(getSpaces(l)+sout);
+
+		
+	}
+	
+	public static String getSpaces(int n){
+		char[] spaces=new char[n];
+		Arrays.fill(spaces, ' ');
+		
+		return String.copyValueOf(spaces);
+	}
+}
