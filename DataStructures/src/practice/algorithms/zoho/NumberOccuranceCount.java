b7f8bbdafc55908458c0a8a96261fc14b4c6f09c
surya <surya@SuryaLinuxMintLenovo.com>
2017-02-25 19:38:00 +0530

    Works like Magic

---
+++ DataStructures/src/practice/algorithms/zoho/NumberOccuranceCount.java
@@ -0,0 +1,32 @@
+package practice.algorithms.zoho;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
+public class NumberOccuranceCount {
+
+	public static void main(String[] args) {
+		int[] a = { 2, 3, 2, 6, 1, 6, 2 };
+
+		HashMap<Integer, Integer> numberCount = new HashMap<Integer, Integer>();
+
+		Integer oldValue = 0;
+
+		numberCount.get(0);
+
+		for (int i = 0; i < a.length; i++) {
+
+			oldValue = numberCount.get(a[i]);
+
+			numberCount.put(a[i], oldValue != null ? oldValue + 1 : 1);
+		}
+
+		Iterator<Entry<Integer, Integer>> i = numberCount.entrySet().iterator();
+
+		while (i.hasNext()) {
+			System.out.println(i.next());
+		}
+	}
+}
