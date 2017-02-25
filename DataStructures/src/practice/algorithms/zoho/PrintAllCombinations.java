1fcb45974df0a2a93d7459d87bcc40d53e59919c
surya <surya@SuryaLinuxMintLenovo.com>
2017-02-26 00:17:06 +0530

    Combinatrics

---
+++ DataStructures/src/practice/algorithms/zoho/PrintAllCombinations.java
@@ -0,0 +1,14 @@
+package practice.algorithms.zoho;
+
+public class PrintAllCombinations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	
+	
+	
+}
--- DataStructures/src/practice/algorithms/zoho/StringCombinations.java
+++ DataStructures/src/practice/algorithms/zoho/StringCombinations.java
@@ -4,10 +4,17 @@ public class StringCombinations {
 
 	static int count = 0;
 
+	static String input;
+	static int len;	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		permute("", "surya");
+		input="abc";
+		len=input.length();
+		
+	//permute("", "surya");
+		for(int i=1;i<=3;i++)
+		permute("",i);
 	}
 
 	public static void permute(String prefix, String input) {
@@ -20,6 +27,22 @@ public class StringCombinations {
 		for (int i = 0; i < len; i++) {
 
 			permute(prefix + "" + input.charAt(i), input.substring(0, i) + input.substring(i + 1, len));
+		}
+
+	}
+	
+
+
+	public static void permute(String prefix, int k) {
+		
+		if (k == 0) {
+			System.out.println(++count + "\t " + prefix);
+			return;
+		}
+
+		for (int i = 0; i < len; i++) {
+
+			permute(prefix + "" + input.charAt(i), k-1);
 		}
 
 	}
