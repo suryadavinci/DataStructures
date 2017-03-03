f95628fa21c951ca747a979de2ad5b56b263af53
gnanasuriyan.g <gnanasuriyan.g@D7240.prodapt.com>
2017-03-03 11:19:04 +0530

    Insert and Search working in Trie

--- DataStructures/src/test/datastructures/Trie.java
+++ DataStructures/src/test/datastructures/Trie.java
@@ -1,69 +1,75 @@
 package test.datastructures;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Trie {
 
 	TrieNode root;
+
 	Trie() {
 		this.root = new TrieNode(' ');
 	}
+
 	public void insert(String word) {
-		TrieNode current=root;
-		
-		for(char c:word.toCharArray()){
-			TrieNode child=current.subNode(c);
-			if(child!=null)
-				current=child;
-			else
-				{
-					current.childList.add(new TrieNode(c));
-					current=current.subNode(c);
-				}
-			
+		TrieNode current = root;
+
+		for (char c : word.toCharArray()) {
+			TrieNode child = current.subNode(c);
+			if (child != null)
+				current = child;
+			else {
+				current.childList.add(new TrieNode(c));
+				current = current.subNode(c);
+			}
+
 		}
-		current.isEnd=true;
+		current.isEnd = true;
 	}
-	public boolean search(String word){
-		TrieNode current=this.root;
-		for(char c:word.toCharArray()){
-			if(current.subNode(c)==null)
+
+	public boolean search(String word) {
+		TrieNode current = this.root;
+		for (char c : word.toCharArray()) {
+			if (current.subNode(c) == null)
 				return false;
 			else
-				current=current.subNode(c);
+				current = current.subNode(c);
 		}
 		return current.isEnd;
 	}
-	
-	
+
 	public static void main(String[] args) {
-		Trie t= new Trie();
-		
-		t.insert("surya");
-		t.insert("suraj");
-
-
-		System.out.println(t.search("sur"));
-		System.out.println(t.search("suraj"));
-		System.out.println(t.search("surya"));
-
-		t.insert("sur");
-		
-		System.out.println(t.search("sur"));
-
-
+		Trie t = new Trie();
+		Scanner s = new Scanner(System.in);
+		boolean exit = false;
+		while (!exit) {
+			System.out.println("1. Insert\n2. Search");
+			switch (s.nextInt()) {
+			case 1:
+				t.insert(s.next());
+				break;
+			case 2:
+				System.out.println(t.search(s.next()));
+				break;
+			default:
+				exit = true;
+				;
+			}
+		}
+		s.close();
 	}
-
 }
 
 class TrieNode {
 	char data;
 	LinkedList<TrieNode> childList;
 	boolean isEnd;
+
 	TrieNode(char c) {
 		childList = new LinkedList<TrieNode>();
 		this.data = c;
 	}
+
 	TrieNode subNode(char c) {
 		if (childList != null)
 			for (TrieNode eachChild : childList)
